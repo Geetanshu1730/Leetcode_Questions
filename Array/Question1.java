d40549b2ee21828ef400a72aea3281dbb9c026cd
Geetanshu1730 <geetanshu751@gmail.com>
2026-07-21 20:12:44 +0530

    Add Maximum Number of Vowels solution
    	#	new file:   Array/Question1.java

---
+++ Array/Question1.java
@@ -0,0 +1,43 @@
+class Question1 {
+    public static boolean isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+    }
+
+    public int maxVowels(String s, int k) {
+        int vowelCount = 0;
+
+        for(int i = 0; i < k; i++){
+            if(isVowel(s.charAt(i))){
+                vowelCount++;
+            }
+        }
+
+        int maxVowel = vowelCount;
+
+        for(int i = k; i < s.length(); i++){
+            if(isVowel(s.charAt(i-k))){
+                vowelCount--;
+            }
+
+            if(isVowel(s.charAt(i))){
+                vowelCount++;
+                maxVowel = Math.max(maxVowel, vowelCount);
+            }
+
+        }
+
+        return maxVowel;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        String s = "abciiidef";
+        int k = 3;
+
+        int ans = obj.maxVowels(s, k);
+
+        System.out.println(ans);
+    }
+
+}
